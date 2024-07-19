4c3bebf96d81186259e7479c614386d7865007c0
Marco Tartaglione <akenatotartaglione@gmail.com>
2024-07-19 15:45:56 +0200

    Common utilities

---
+++ Commons.h
@@ -0,0 +1,11 @@
+//
+// Created by Marco on 18/07/2024.
+//
+
+#ifndef WOLFENSTEIN3D_COMMONS_H
+#define WOLFENSTEIN3D_COMMONS_H
+
+#define FONT_BG_COLOR (0xF00000)
+#define FONT_DIRECTORY "assets/font/"
+
+#endif //WOLFENSTEIN3D_COMMONS_H
